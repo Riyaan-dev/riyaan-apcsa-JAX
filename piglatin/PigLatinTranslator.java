9958067cb7c6f7885980ca66f7f71e86b780859d
Riyaan Amin <riyaanamin82@gmail.com>
2025-10-15 21:40:17 +0000

    Started project (cooked)

--- piglatin/PigLatinTranslator.java
+++ piglatin/PigLatinTranslator.java
@@ -23,13 +23,84 @@ public class PigLatinTranslator {
         // This method must call translateWord once for each word in the string.
         result = translateWord(input);
 
+        
+
         return result;
     }
 
     private static String translateWord(String input) {
         System.out.println("  -> translateWord('" + input + "')");
 
-        String result = "";
+        if (input == null) {
+            return null;
+        }
+        else if (input.length() == 0) {
+            return "";
+        }
+
+        else if (input.trim().length() == 0) {
+            return input;
+        }
+
+        else if (input.contains(" ")) {
+            String[] words = input.split(" ");
+            String result = "";
+            for (String word : words) {
+                result += translateWord(word) + " ";
+            }
+            return result.trim();
+        }
+        else if (!Character.isLetter(input.charAt(0))) {
+            return input;
+        }
+
+        else if (!Character.isLetter(input.charAt(input.length() - 1))) {
+            char lastChar = input.charAt(input.length() - 1);
+            String wordWithoutLastChar = input.substring(0, input.length() - 1);
+            return translateWord(wordWithoutLastChar) + lastChar;
+        }
+
+        else if (input.length() == 1 && Character.isLetter(input.charAt(0))) {
+            return input + "ay";
+        }
+
+        public static boolean isVowel(char c) {
+            char lowerC = Character.toLowerCase(c);
+            return lowerC == 'a' || lowerC == 'e' || lowerC == 'i' || lowerC == 'o' || lowerC == 'u';
+        }
+
+        if (isVowel(input.charAt(0))) {
+            return input + "way";
+        }
+        else {
+            int firstVowelIndex = -1;
+            for (int i = 0; i < input.length(); i++) {
+                if (isVowel(input.charAt(i))) {
+                    firstVowelIndex = i;
+                    break;
+                }
+            }
+            if (firstVowelIndex == -1) {
+                return input + "ay";
+            } else {
+                String consonantCluster = input.substring(0, firstVowelIndex);
+                String restOfWord = input.substring(firstVowelIndex);
+                return restOfWord + consonantCluster + "ay";
+            }
+        }
+        return result;
+
+        String result = input;
+
+    if (input.length() == 0) {
+        return input;
+    }
+
+    else if (input.length() == 1) {
+        return input + "ay";
+    }
+
+
 
         // TODO: Replace this code to correctly translate a single word.
         // Start here first!
