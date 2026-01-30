764db1183bec4c8b494c2250fd9855198b37285b
Riyaan Amin <riyaanamin82@gmail.com>
2026-01-30 19:14:28 +0000

    started selection sort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -3,6 +3,24 @@ package sorting;
 public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
+
+        for (int i=0; i<input.length;i++){
+            int mIndex = i;
+
+            for (int j = 1; j< input.length-1;j++){
+                if (input[j] < input[mIndex]){
+                    mIndex=j;
+                }
+            }
+        }  
+
+
+
+
+
+
+
+
         System.out.println("Selection Sort!!!");
     }
 }
