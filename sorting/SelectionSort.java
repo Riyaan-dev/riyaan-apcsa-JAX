b0254b44ad4bb302e051ed82a531045dec135dbf
Riyaan Amin <riyaanamin82@gmail.com>
2026-01-30 19:17:33 +0000

    save

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -5,15 +5,22 @@ public class SelectionSort implements Sorter {
     public void sort(int[] input) {
 
         for (int i=0; i<input.length;i++){
+            
             int minimumIndex = i;
+
+
 
             for (int j = i+1; j< input.length;j++){
                 if (input[j] < input[minimumIndex]){
                     minimumIndex=j;
+
+
+
                 }
             }
             int temp = input[i];
             input[i] = input[minimumIndex];
+
             input[minimumIndex] = temp;
         }  
 
