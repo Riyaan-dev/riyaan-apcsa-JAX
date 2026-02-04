f055780a0591bc4b2025a9bf963776fa62b076b7
Riyaan Amin <riyaanamin82@gmail.com>
2026-02-04 03:26:17 +0000

    final product with steps

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -3,20 +3,33 @@ package sorting;
 public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
-        for (int i =1; i < input.length; i++){
+        int steps = 0;
+
+        for (int i = 1; i < input.length; i++) {
+
+
             int key = input[i];
-            int j = i-1;
 
-            while (j >= 0 && input[j] > key){
-                input [j+1] = input[j];
+
+            int j = i - 1;
+            steps++; 
+
+
+
+            while (j >= 0 && input[j] > key) {
+                steps++; 
+
+                input[j + 1] = input[j];
+
                 j--;
+                steps += 2; 
             }
-            input[j+1] = key;
 
+            input[j + 1] = key;
+
+
+            steps++; 
         }
-    
-    // done
     }
     
 }
-
--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -3,34 +3,32 @@ package sorting;
 public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
+        int steps = 0;
 
-        for (int i=0; i<input.length;i++){
-            
-            int minimumIndex = i;
+        for (int i = 0; i < input.length - 1; i++) {
+            int minIndex = i;
 
-
-
-            for (int j = i+1; j< input.length;j++){
-                if (input[j] < input[minimumIndex]){
-                    minimumIndex=j;
+            for (int j = i + 1; j < input.length; j++) {
+                steps++; 
 
 
+                if (input[j] < input[minIndex]) {
 
+                    minIndex = j;
                 }
             }
+
             int temp = input[i];
-            input[i] = input[minimumIndex];
-
-            input[minimumIndex] = temp;
-        }  
 
 
+            input[i] = input[minIndex];
 
 
+            input[minIndex] = temp;
+            steps += 3; 
 
+        }
 
-
-
-  
+        
     }
 }
