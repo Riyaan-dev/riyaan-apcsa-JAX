031ef4ca9bfdddd901f8786a7b9e42a0a75ef08b
Riyaan Amin <riyaanamin82@gmail.com>
2026-04-03 23:02:05 +0000

    finished towersolver

--- tower/TowerSolver.java
+++ tower/TowerSolver.java
@@ -1,6 +1,7 @@
 package tower;
 
 public class TowerSolver {
+
     private TowerModel model;
 
     public TowerSolver()
@@ -11,25 +12,20 @@ public class TowerSolver {
     public void solve(TowerModel model)
     {
         this.model = model;
-        solve(model, 0, 1, 2, model.height()-1);
-
-        
+        solve(model, 0, 1, 2, model.height() - 1);
     }
 
-    // Create an overloaded solve(...) method
-    // This new method will be recursive (call itself)
-    //
-    public void solve(TowerModel model, int start, int goal, int extra, int layer)  {
-        if(layer==0){
+    // Recursive method
+    public void solve(TowerModel model, int start, int goal, int extra, int layer)
+    {
+        if (layer == 0)
+        {
             model.move(start, goal);
             return;
         }
-        solve(model, start, extra, goal, layer-1);
 
-        
-        model.move(start,goal);
-    
-        solve(model, extra,goal,start,layer-1);
+        solve(model, start, extra, goal, layer - 1);
+        model.move(start, goal);
+        solve(model, extra, goal, start, layer - 1);
     }
-
 }
