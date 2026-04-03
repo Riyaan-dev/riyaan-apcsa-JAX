e19d2e865291a8934533bac0d3db53e2b92a19a3
Riyaan Amin <riyaanamin82@gmail.com>
2026-04-03 23:01:12 +0000

    first methods comlete

--- tower/TowerModel.java
+++ tower/TowerModel.java
@@ -2,82 +2,84 @@ package tower;
 
 public class TowerModel {
 
-    // 2d array storing game state
     private IntegerStack[] towers;
-
-    // Final tower height
     private int towerHeight = 0;
 
-    // Debug metrics
     private int printCounter = 0;
     private int moveCounter = 0;
 
-    /* This class implements a model of a tower of Hanoi game.
-
-        |    |    |
-        =    |    |
-       ===   |    |
-      =====  |    |
-     ------------------
-        0    1    2
-
-     Example of a game of height three in the starting position.
-
-    */
     public TowerModel(int height)
     {
         towerHeight = height;
+
         towers = new IntegerStack[3];
         towers[0] = new IntegerStack(height);
         towers[1] = new IntegerStack(height);
         towers[2] = new IntegerStack(height);
 
-        for (int i=0; i<height; i++)
+        for (int i = 0; i < height; i++)
         {
             towers[0].push(height - i);
         }
     }
 
-    // get the total number of disks
     public int height()
     {
         return towerHeight;
     }
 
-
-    // Move one disk from the source stack to the destination stack.
+    // ✅ FIXED move method (prevents illegal moves)
     public void move(int source, int destination)
     {
-        System.out.println("Move #" + ++moveCounter + " from " + source + " to " + destination);
-        // TODO!!
+        int disk = towers[source].peek();
+
+        if (disk != 0)
+        {
+            int destTop = towers[destination].peek();
+
+            // Only allow valid move
+            if (destTop == 0 || destTop > disk)
+            {
+                towers[source].pop();
+                towers[destination].push(disk);
+                moveCounter++;
+            }
+        }
     }
 
-    // Helper method to nicely print the current model state.
     public void print()
     {
         System.out.print("Print #" + ++printCounter + " Towers of Hanoi\n");
 
-        for (int layer = towerHeight-1; layer >= 0; layer--){
+        for (int layer = towerHeight - 1; layer >= 0; layer--)
+        {
             System.out.print("\n");
-            for (int tower = 0; tower < towers.length; tower++) {
+
+            for (int tower = 0; tower < towers.length; tower++)
+            {
                 int value = towers[tower].get(layer);
+
                 if (value == 0)
                 {
                     System.out.print(" |");
-                } else
+                }
+                else
                 {
                     System.out.print(" " + value);
                 }
             }
         }
+
         System.out.println();
-        for (int i = 0; i < towers.length; i++) {
+
+        for (int i = 0; i < towers.length; i++)
+        {
             System.out.print(" =");
         }
+
         System.out.println();
     }
 
-    // Test instrumentation
     public IntegerStack[] getTowers()
     {
         return towers;
--- tower/TowerSolver.java
+++ tower/TowerSolver.java
@@ -11,13 +11,24 @@ public class TowerSolver {
     public void solve(TowerModel model)
     {
         this.model = model;
-        // Call the missing solve method (not this one)
+        solve(model, 0, 1, 2, model.height()-1);
+
+        
     }
 
     // Create an overloaded solve(...) method
     // This new method will be recursive (call itself)
     //
-    // [ solve method here]
-    //
+    public void solve(TowerModel model, int start, int goal, int extra, int layer)  {
+        if(layer==0){
+            model.move(start, goal);
+            return;
+        }
+        solve(model, start, extra, goal, layer-1);
+
+        model.move(start,goal);
+    
+        solve(model, extra,goal,start,layer-1);
+    }
 
 }
